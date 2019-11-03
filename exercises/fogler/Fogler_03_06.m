9e25fba93982d4749495b89f9477f7c2c0a98117
Alberto Cuoci <alberto.cuoci@gmail.com>
2019-11-03 11:30:48 +0100

    Update Fogler_03_06.m

--- exercises/fogler/Fogler_03_06.m
+++ exercises/fogler/Fogler_03_06.m
@@ -17,9 +17,9 @@
 %                                                                         |
 %   This file is part of Matlab4CRE framework.                            |
 %                                                                         |
-%	License                                                               |
+%   License                                                               |
 %                                                                         |
-%   Copyright(C) 2014 Alberto Cuoci                                       |
+%   Copyright(C) 2019 Alberto Cuoci                                       |
 %   Matlab4CRE is free software: you can redistribute it and/or modify    |
 %   it under the terms of the GNU General Public License as published by  |
 %   the Free Software Foundation, either version 3 of the License, or     |
@@ -34,18 +34,17 @@
 %   along with Matlab4CRE. If not, see <http://www.gnu.org/licenses/>.    |
 %                                                                         |
 %-------------------------------------------------------------------------%
-
+%                                                                         %
+% Fogler, Exercise 3-6:  Calculating the equilibrium conversion           %
+%                                                                         %
+% Reaction: A <-> 2B                                                      %
+% r = k*(CA - CB^2/KC)                                                    %
+%                                                                         %
 %-------------------------------------------------------------------------%
-% Fogler, Exercise 3-6:  Calculating the equilibrium conversion
-%
-% Reaction: A <-> 2B 
-% r = k*(CA - CB^2/KC)
-%-------------------------------------------------------------------------%
-clear all;
+close all; clear variables;
 	
-%-------------------------------------------------------------------------%
-% Data
-%-------------------------------------------------------------------------%
+
+%% Data
 eps = 1;		% changes of moles
 P = 202.6e3;	% pressure [Pa]
 T = 340;		% temperature [K]
@@ -54,9 +53,7 @@ k1 = 0.5;		% forward kinetic constant [1/s]
 CA0 = P/8314/T; % initial concentration [kmol/m3]=[mol/dm3]
 
 
-%-------------------------------------------------------------------------%
-% 1. Batch Reactor (BR)
-%-------------------------------------------------------------------------%
+%% Batch Reactor (BR)
 equilibriumXBatch = @(x) 4*CA0*x^2+x*KC-KC;  % parametrized function
 fun = @(x) equilibriumXBatch(x);    		 % function of x alone
 xBatch = fzero(fun,0.2);
@@ -66,9 +63,8 @@ batch_minus_RA = k1*CA0*((1-XBatch)-4*CA0*XBatch.^2/KC);
 batch_CA = CA0*(1-XBatch);
 batch_CB = 2*CA0*XBatch;
 
-%-------------------------------------------------------------------------%
-% 2. Flow Reactors
-%-------------------------------------------------------------------------%
+
+%% Flow Reactors
 equilibriumXFlow = @(x) 4*CA0*x^2-KC*(1-x)*(1+eps*x);  	% parametrized function
 fun = @(x) equilibriumXFlow(x);    						% function of x alone
 xFlow = fzero(fun,0.2);
@@ -78,49 +74,39 @@ flow_minus_RA = k1*CA0./(1+eps*XFlow).*((1-XFlow)-4*CA0*XFlow.^2/KC./(1+eps*XFlo
 flow_CA = CA0*(1-XFlow)./(1+eps*XFlow);
 flow_CB = 2*CA0*XFlow./(1+eps*XFlow);
 
-%-------------------------------------------------------------------------%
-% Plots
-%-------------------------------------------------------------------------%
+
+%% Plots
+
 figure; % create new figure
 
 subplot(2,2,1);
 hold all;
 plot(XBatch,1./batch_minus_RA, 'LineWidth',2);
 plot(XFlow,1./flow_minus_RA,'LineWidth',2);
-title('Levenspiel Plot');
-xlim([0 0.5]);
-ylim([0 1000]);
-xlabel('conversion');
-ylabel('-1/R_A [dm^3.min/mol]');
-legend('Batch','Flow');
+title('Levenspiel Plot');   legend('Batch','Flow');
+xlim([0 0.5]);  ylim([0 1000]);
+xlabel('conversion');   ylabel('-1/R_A [dm^3min/mol]');
 
 subplot(2,2,2);
 hold all;
 plot(XBatch,batch_minus_RA,'LineWidth',2);
 plot(XFlow,flow_minus_RA,'LineWidth',2);
-title('Reaction rate plot');
-xlim([0 0.5]);
-%ylim([0 1000]);
-xlabel('conversion');
-ylabel('r [mol/dm^3/min]');
-legend('Batch','Flow');
+title('Reaction rate plot'); legend('Batch','Flow');
+xlim([0 0.5]);  %ylim([0 1000]);
+xlabel('conversion');   ylabel('r [mol/dm^3/min]');
 
 subplot(2,2,3);
 hold all;
 plot(XBatch,batch_CA,'LineWidth',2);
 plot(XFlow,flow_CA,'LineWidth',2);
-title('Concentration species A');
+title('Concentration species A');   legend('Batch','Flow');
 xlim([0 0.5]);
-xlabel('conversion');
-ylabel('CA [mol/dm^3]');
-legend('Batch','Flow');
+xlabel('conversion');   ylabel('CA [mol/dm^3]');
 
 subplot(2,2,4);
 hold all;
 plot(XBatch,batch_CB,'LineWidth',2);
 plot(XFlow,flow_CB,'LineWidth',2);
-title('Concentration species B');
+title('Concentration species B');legend('Batch','Flow');
 xlim([0 0.5]);
-xlabel('conversion');
-ylabel('CB [mol/dm^3]');
-legend('Batch','Flow');
+xlabel('conversion');   ylabel('CB [mol/dm^3]');
