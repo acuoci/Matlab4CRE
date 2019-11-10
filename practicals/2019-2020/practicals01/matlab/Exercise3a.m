8a955f3301fabba0c4006a428b8e546dde513dcb
Alberto Cuoci <alberto.cuoci@gmail.com>
2019-11-10 11:31:07 +0100

    Updated structure of practicals

--- practicals/2019-2020/practicals01/matlab/Exercise3a.m
+++ practicals/2019-2020/practicals01/matlab/Exercise3a.m
@@ -35,96 +35,70 @@
 %                                                                         |
 %-------------------------------------------------------------------------%
 %                                                                         %
-% Isothermal, batch reactor (constant density)                            %
+% Non isothermal, adiabatic plug flow reactor (constant density)          %
 % Requires at least MATLAB-2016b                                          %
 %                                                                         %
 %-------------------------------------------------------------------------%
-close all;	clear variables;
-
-%% Global variables
-global k1;
-global k2;
-
+close all;  clear variables;
 
 %% Data
-A1=1.50E+04;	% [1/h]
-E1=9000;        % [cal/mol]
-A2=6.00E+06;	% [1/h]
-E2=19000;       % [cal/mol]
-		
-T=500;	% [K]
-V=0.5;	% [m3]
-NA0=20;	% [kmol]
-TauD=1;	% [h]
+Tin = 24+273.15; % [K]
+T0  = 20+273.15; % [K]
+Tcr = 53+273.15; % [K]
+
+V = 1135;        % [l]
+
+FA0 = 19.50;     % [kmol/h]
+FB0 = 364;       % [kmol/h]
+FC0 = 0;         % [kmol/h]
+FM0 = 32.60;     % [kmol/h]
+
+QA0 = 1320;      % [m3/h]
+QB0 = 6600;      % [m3/h]
+QC0 = 0;         % [m3/h]
+QM0 = 1320;      % [m3/h]
+
+A = 16.96e12;    % [1/h]
+E = 72000;       % [J/mol]
+
+CpA = 146;       % [J/mol/K]
+CpB = 75;        % [J/mol/K]
+CpC = 192;       % [J/mol/K]
+CpM = 82;        % [J/mol/K]
+
+HA0 = -148918;   % [J/mol]
+HB0 = -275000;   % [J/mol]
+HC0 = -505360;   % [J/mol]
 
 
-%% Preliminary calculations
+%% Calculations
+Qtot = QA0+QB0+QC0+QM0; % [m3/s]
+CA0 = FA0/Qtot;         % [kmol/m3]
+CB0 = FB0/Qtot;         % [kmol/m3]
+CC0 = FC0/Qtot;         % [kmol/m3]
+CM0 = FM0/Qtot;         % [kmol/m3]
 
-% Kinetic constants
-k1=A1*exp(-E1/1.987/T);  %[1/h]
-k2=A2*exp(-E2/1.987/T);  %[1/h]
+TetaA0 = CA0/CA0;       % [-]
+TetaB0 = CB0/CA0;       % [-]
+TetaC0 = CC0/CA0;       % [-]
+TetaM0 = CM0/CA0;       % [-]
 
-% Initial conditions
-CA0 = NA0/V;    % [kmol]
-CB0 = 0.;       % [kmol]
-CC0 = 0.;       % [kmol]
-tau = 3;        % [h]
+CpIn = TetaA0*CpA+TetaB0*CpB+TetaC0*CpC+TetaM0*CpM;  % [J/mol/K]
+deltaCp = CpC-CpA-CpB;                               % [J/mol/K]
+deltaHR0 = HC0-HA0-HB0;                              % [J/mol]
 
-
-%% ODE solution
-C0 = [CA0 CB0 CC0]';
-[t, C] = ode45(@isothermalBatch, [0 tau], C0);
+Tau = V/Qtot;   % [h]
 
 
-%% Post processing
-CA = C(:,1);                % [kmol/m3]
-CB = C(:,2);                % [kmol/m3]
-CC = C(:,3);                % [kmol/m3]
-PB = CB*V*24./(TauD+t);     % [kmol/day]
+%% Graphical solution                                              %
+T = 40:100;
+T = T +273.15;
+Xmb = A*exp(-E/8.314./T)*Tau./(1+A*exp(-E/8.314./T)*Tau);
+Xeb = -CpIn*(T-Tin)./(deltaHR0+deltaCp*(T-T0));
 
-% Concentrations
 figure;
-plot(t,CA,'-o', t,CB,'-o', t,CC,'-o');
-legend('CA', 'CB', 'CC');
-xlabel('time [h]');
-ylabel('C [kmol/m3]');
-title('Concentrations');
-
-[maxCB, iMaxCB] = max(CB);
-tauMaxCB = t(iMaxCB);
-fprintf('Optimization: Max CB($): %f @ Tau(s)=%f \n', ...
-         maxCB, tauMaxCB);
-
-%Production
-figure;
-plot(t,PB,'-o');
-xlabel('time [h]');
-ylabel('PB [kmol/day]');
-title('Production');
-
-[maxPB, iMaxPB] = max(PB);
-tauMaxPB = t(iMaxPB);
-fprintf('Optimization: Max PB($): %f @ Tau(s)=%f \n', ...
-         maxPB, tauMaxPB);
-
-
-
-%% ODE system (defined locally, requires at least MATLAB-2016b)
-function dC = isothermalBatch(t,C)
-
-    global k1;      % [1/h]
-    global k2;      % [1/h]
-
-    CA = C(1);      % [kmol/m3]
-    CB = C(2);      % [kmol/m3]
-
-    r1 = k1*CA;     % [kmol/m3/h]
-    r2 = k2*CB;     % [kmol/m3/h]
-
-    dCA = -r1;      % [kmol/m3/h]
-    dCB = r1-r2;    % [kmol/m3/h]
-    dCC = r2;       % [kmol/m3/h]
-
-    dC = [dCA dCB dCC]';
-
-end
+plot((T-273.15),Xmb,'-o', (T-273.15), Xeb,'-o');
+legend('Xmb', 'Xeb');
+xlabel('T [C]');
+ylabel('X');
+title('Main results');
